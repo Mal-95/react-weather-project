259f509b869a3c4cb7e4ce52503354befee8372b
Mal-95 <malebo445.ml@gmail.com>
2024-05-28 15:28:32 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
+      <footer>This project was coded by{" "}
+      <a href="https://github.com/Mal-95" target="_blank" rel="noreferrer">Malebo Letsoalo,</a>
+      {" "}open-sourced on{" "}
+      <a href="https://github.com/Mal-95/react-weather-project" target="_blank" rel="noreferrer">GitHub</a>
+      {" "}and hosted on{" "}
+      <a href="https://app.netlify.com/sites/react-weather-app-project-2024" target="_blank" rel="noreferrer">Netlify</a>
+      </footer>
+      </div>
     </div>
   );
 }
