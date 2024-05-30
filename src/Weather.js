e4d5f88b646ac842268c20c5bb3999d735bab6ff
Mal-95 <malebo445.ml@gmail.com>
2024-05-30 18:40:36 +0200

    Added content to weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                      <input type="search" placeholder="Type a city..." className="form-control" />
+                      <input type="search" placeholder="Type a city..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                      <input type="submit" value="search" className="btn btn-primary" /> 
+                      <input type="submit" value="Search" className="btn btn-primary w-100" /> 
                     </div>
                 </div>
             </form>
@@ -22,7 +22,8 @@ export default function Weather() {
             <div className="row">
                 <div className="col-6">
                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Cloudy" />
-                    21°C
+                    <span className="temperature">21</span>
+                    <span className="unit">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
